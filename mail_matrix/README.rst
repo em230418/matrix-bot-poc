02134f355185cb48f3190eb500b17ef537445df1
Eugene Molotov <molotov@it-projects.info>
2024-11-26 11:14:03 +0500

    saved

---
+++ mail_matrix/README.rst
@@ -0,0 +1,25 @@
+===========================
+ Matrix Server Integration
+===========================
+
+_
+
+__
+
+Credits
+=======
+
+Contributors
+------------
+
+* `Eugene Molotov <https://github.com/em230418>`__
+
+Sponsors
+--------
+
+* `IT-Projects LLC <https://it-projects.info>`__
+
+Maintainers
+-----------
+
+* `IT-Projects LLC <https://it-projects.info>`__
---
+++ mail_matrix/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": """Matrix Server Integration""",
+    "version": "15.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://www.it-projects.info",
+    "license": "LGPL-3",
+    "depends": [
+        'mail,queue_job',
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
